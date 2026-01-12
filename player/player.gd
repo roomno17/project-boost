22fc67d1b6a76b519d3b1d93d2f56166a213685b
roomno17 <2261021@flourbluffschools.org>
2026-01-12 12:41:11 -0600

    added sound

--- player/player.gd
+++ player/player.gd
@@ -3,6 +3,12 @@ class_name Player
 @export_range(750,2500) var force :=1000.0
 @export var torque := 100
 var transitioning :=false
+@onready var success: AudioStreamPlayer = $Success
+@onready var explosion: AudioStreamPlayer = $Explosion
+@onready var rocket: AudioStreamPlayer3D = $Rocket
+
+
+
 func _ready() -> void:
 	pass
 
@@ -10,6 +16,7 @@ func _process(delta: float) -> void:
 	if not transitioning:
 		if Input.is_action_pressed("space"):
 			apply_central_force(basis.y *delta *force)
+			rocket.play()
 		if Input.is_action_pressed("rleft"):
 			apply_torque(Vector3(0,0,delta*torque))
 		if Input.is_action_pressed("rright"):
@@ -23,11 +30,13 @@ func _on_body_entered(body: Node) -> void:
 			crash_sequence()
 		
 func crash_sequence():
+	explosion.play()
 	transitioning=true
 	await get_tree().create_timer(1).timeout
 	get_tree().reload_current_scene.call_deferred()
 
 func complete_level(next_level):
+	success.play()
 	transitioning=true
 	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file.call_deferred(next_level)
